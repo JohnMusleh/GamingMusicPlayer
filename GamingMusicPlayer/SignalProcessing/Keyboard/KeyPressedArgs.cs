0ff7b58db0ef51a15ec7f2c6b4f708da82317f21
alkerr <john.musleh@gmail.com>
2018-12-26 12:24:05 +0200

    organizing files

---
+++ GamingMusicPlayer/SignalProcessing/Keyboard/KeyPressedArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Diagnostics;
+
+namespace GamingMusicPlayer
+{
+    public class KeyPressedArgs : EventArgs
+    {
+        public Key KeyPressed { get;}
+        public Boolean Down { get;}
+        public KeyPressedArgs(Key k,Boolean d)
+        {
+            KeyPressed = k;
+            Down = d;
+        }
+
+        public override string ToString() {
+            if (Down)
+                return KeyPressed.ToString()+" Down";
+            return KeyPressed.ToString() + " Up"; ;
+        }
+
+
+    }
+}
